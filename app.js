ad809113cac267dea897457d7f24f93d16b80d31
Vicky <vickygupta9987@gmail.com>
2022-04-23 21:26:34 +0530

    Initialize commit

--- app.js
+++ app.js
@@ -141,12 +141,6 @@ app.get("/about", (req, res) => {
     res.render("about");
 })
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
-
-app.listen(port, function(){
-    console.log(`Server started on port ${port}`);
+app.listen(process.env.PORT || 3000, function(){
+    console.log(`Server started on port 3000`);
 })
